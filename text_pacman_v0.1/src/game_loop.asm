7fc4cecba12ef00a4320117ca476eb7b354d0d1d
pumpkinbee <juneson0304@naver.com>
2026-02-25 07:43:27 +0000

    v0.3.3: 좌표 기반 직접 커서 제어 방식

--- text_pacman_v0.1/src/game_loop.asm
+++ text_pacman_v0.1/src/game_loop.asm
@@ -13,73 +13,55 @@
 ; 작성자    :   남궁명수
 ; ============================
 global main 
+
 extern draw_map
-extern map_data
-extern map_width
-extern map_height
-extern map_size
 extern read_input 
 extern input_char
-extern player_x 
-extern player_y
 extern enable_raw_mode 
 extern disable_raw_mode
 
+extern draw_player 
+extern erase_player 
+extern update_player 
+extern player_x 
+extern player_y
+
+
 section .data 
-; clear_screen db 27, '[', '2', 'J', 27, '[', 'H'
-; clear_screen db 0x1B, '[', 'H', 0x1B, '[', 'J'  
-; clear_len equ $ - clear_screen
-cursor_home db 27, '[', 'H' 
-cursor_len equ $ - cursor_home
+clear_screen db 27, '[', '2', 'J', 27, '[', 'H'
+clear_len equ $ - clear_screen
 
-LF equ 10 
-STDIN equ 0 
 STDOUT equ 1 
-
-SYS_read equ 0 	     ; read 
 SYS_write equ 1 	; write 
 SYS_exit equ 60 	; terminate 
 
 section .text 
-; ====================
-;   플레이어 위치 계산
-;   rax = y 
-;   rbx = x 
-;   반환: rax = index 
-; ====================
-calc_index:
-    mov rcx, qword [map_width]
-    inc rcx     ; LF 포함
-    imul rax, rcx 
-    add rax, rbx 
-    ret 
-
-
 main: 
     call enable_raw_mode
-
-game_loop:
+    
     ; --------------------
     ;   화면 클리어
     ; --------------------
     mov rax, SYS_write 
     mov rdi, STDOUT
-    mov rsi, cursor_home
-    mov rdx, cursor_len
+    mov rsi, clear_screen
+    mov rdx, clear_len
     syscall 
-    
+
     ; --------------------
-    ;   플레이어 'P' 삽입
+    ;   맵은 1번만 출력
     ; --------------------
-    mov rax, qword [player_y]
-    mov rbx, qword [player_x]
-    call calc_index 
-    mov byte[map_data + rax], 'P'
-    
+    call draw_map
+
     ; --------------------
-    ;   맵 출력
+    ;   플레이어 최초 출력
     ; --------------------
-    call draw_map 
+    call draw_player
+
+; ====================
+;   게임 루프
+; ====================
+game_loop:
 
     ; --------------------
     ;   입력 읽기
@@ -94,116 +76,26 @@ game_loop:
     cmp r11b, 'q' 
     je exit_program
 
+    ; 플레이어 지우기 
+    call erase_player 
 
-    ; --------------------
-    ;   이동 계산용 현재 좌표 복사
-    ; --------------------
-    mov rax, qword [player_y]
-    mov rbx, qword [player_x]
+    ; 이동 처리(현재 오른쪽만)
+    call update_player 
+
+    ; 다시 그리기
+    call draw_player
     
-    ; --------------------
-    ;   방향 판별 (W, A, S, D)
-    ; --------------------
-    cmp r11b, 'w' 
-    je try_up
-
-    cmp r11b, 's' 
-    je try_down
-
-    cmp r11b, 'd' 
-    je try_right
-
-    cmp r11b, 'a' 
-    je try_left 
-    
-    jmp game_loop
-
-
-; ====================
-;   이동 시도
-; ====================
-try_up: 
-    cmp rax, 0 
-    je game_loop 
-    dec rax
-    jmp try_move
-
-try_down:
-    mov rcx, [map_height]
-    dec rcx
-    cmp rax, rcx 
-    je game_loop
-    inc rax
-    jmp try_move 
-
-
-try_right: 
-    mov rcx, [map_width]
-    dec rcx
-    cmp rbx, rcx
-    je game_loop
-    inc rbx 
-    jmp try_move
-
-try_left:
-    cmp rbx, 0
-    je game_loop
-    dec rbx
-    jmp try_move 
-
-
-; ====================
-;   벽 충돌 검사
-;   rax = new_y 
-;   rbx = new_x
-; ====================
-try_move:
-    ; new 좌표 백업
-    push rax  
-    push rbx   
-
-    ; index 계산
-    call calc_index 
-
-    ; 벽 검사
-    mov dl, [map_data + rax]
-    cmp dl, '#'
-    je cancel_move 
-
-    ; --------------------
-    ;   이동 성공
-    ; --------------------
-    ; 기존 위치 삭제
-    mov rax, [player_y]
-    mov rbx, [player_x]
-    call calc_index
-    mov byte [map_data + rax], ' '
-
-    ; 새 좌표 복원
-    pop rbx 
-    pop rax 
-
-    ; 이동 확정
-    mov qword [player_y], rax
-    mov qword [player_x], rbx
-
-    jmp game_loop
-
-
-; ====================
-;   이동 취소
-; ====================
-cancel_move:
-    pop rbx 
-    pop rax 
-    jmp game_loop
+    jmp game_loop 
 
 ; ====================
 ;   정상 종료
-; ====================
+; ====================   
 exit_program:
     call disable_raw_mode
-    
+
     mov rax, SYS_exit 
-    xor rdi, rdi  ; 이거 왜 해주는지 아직 모름, 문서 확인하기
+    xor rdi, rdi 
     syscall 
+
+
+
--- text_pacman_v0.1/src/player.asm
+++ text_pacman_v0.1/src/player.asm
@@ -1,15 +1,105 @@
 ; =========================================
 ; 파일명    : player.asm
-; 목적      : 플레이어 좌표
+; 목적      : 플레이어 렌더링 + 좌표 관리
 ; 설명      : 
 ;   - 이동을 위한 좌표
 ;   - 맵, 방해물 등등 요소를 고려해야함
 ; 시작일    : 2026-02-22
 ; 작성자    :   남궁명수
 ; ============================
-global player_y
+global draw_player 
+global erase_player 
+global update_player 
 global player_x 
+global player_y
 
 section .data 
-player_y dq 1
-player_x dq 1
+player_char db 'P'
+space_char db ' '
+
+; 좌표는 0~9 가정(한 자리 숫자)
+player_x db 2
+player_y db 2
+
+STDOUT equ 1 
+SYS_write equ 1 	; write 
+
+CURSOR_LEN equ 6    ; ESC[y;xH
+
+section .bss
+cursor_buf resb 16
+
+section .text 
+
+; ====================
+;   move_cursor(y, x) : ANSI Escape Code를 활용한 커서 이동
+;   rdi = y (0~9)
+;   rsi = x (0~9) 
+; ====================
+move_cursor:
+    mov byte [cursor_buf], 27   ;ESC 
+    mov byte [cursor_buf+1], '[' 
+    
+    ; y 반환
+    add dil, '0' 
+    mov [cursor_buf+2], dil 
+
+    mov byte [cursor_buf+3], ';'
+
+    ;x 반환 
+    add sil, '0' 
+    mov [cursor_buf+4], sil 
+
+    mov byte [cursor_buf+5], 'H' 
+
+    mov rax, SYS_write 
+    mov rdi, STDOUT 
+    lea rsi, [cursor_buf] 
+    mov rdx, CURSOR_LEN
+    syscall 
+
+    ret 
+
+
+; ====================
+;   draw_player(y, x) : 플레이어 그리기
+;   rdi = y 
+;   rsi = x  
+; ====================
+draw_player: 
+    movzx rdi, byte [player_y]
+    movzx rsi, byte [player_x]
+    call move_cursor    ; 해당 좌표로 커서 이동
+
+    mov rax, SYS_write
+    mov rdi, STDOUT 
+    lea rsi, [player_char]
+    mov rdx, 1
+    syscall 
+    ret 
+
+; ====================
+;   erase_player : 이전 플레이어 위치 삭제
+;   rdi = y 
+;   rsi = x  
+; ====================
+erase_player:
+    movzx rdi, byte [player_y]
+    movzx rsi, byte [player_x]
+    call move_cursor 
+
+    mov rax, SYS_write
+    mov rdi, STDOUT
+    lea rsi, [space_char]
+    mov rdx, 1 
+    syscall 
+    ret 
+
+; ====================
+;   update_player
+;   임시: 오른쪽 이동 테스트
+; ====================
+update_player:
+    inc byte [player_x]
+    ret 
+    
